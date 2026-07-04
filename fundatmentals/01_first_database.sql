18164246999e030f005915181f1894c5e9e722f3
Himanshu Mittal <himanshu1210mittal@gmail.com>
2026-07-05 00:36:42 +0530

    first database

---
+++ fundatmentals/01_first_database.sql
@@ -0,0 +1,8 @@
+
+-- delete a db name postgresql_part1 if exists
+DROP DATABASE IF EXISTS postgresql_part1;
+
+
+
+-- create a new database inside ur postgres server
+CREATE DATABASE postgresql_part1;
